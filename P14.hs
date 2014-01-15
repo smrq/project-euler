f0720113f23facb92f299afb0bee3dda63326297
Greg Smith <smr.kyu@gmail.com>
2014-01-15 15:33:06 -0600

    Solved P14

--- P14.hs
+++ P14.hs
@@ -1,12 +1,24 @@
 module P14 where
+import Data.Array
+import Data.List (maximumBy)
+import Data.Ord (comparing)
 main = print result
 
-collatz :: Int -> Int
-collatz = (map collatz' [0 ..] !!) where
-	collatz' 1 = 1
-	collatz' n =
-		if even n
-			then 1+collatz (n `div` 2)
-			else 1+collatz (3*n+1)
+-- Naive
+--collatz 1 = 1
+--collatz n
+--	| even n = 1 + (collatz (n `div` 2))
+--	| otherwise = 1 + (collatz (n*3 + 1))
 
-result = maximum (map collatz [1..1000000])
+-- Memoized
+memoLimit = 1000000
+collatzMemo = array (1, memoLimit) [(n, collatz n) | n <- [1..memoLimit]]
+
+collatz :: Integer -> Int
+collatz 1 = 1
+collatz n = 1 + recur nextN
+	where
+		nextN = if even n then (n `div` 2) else (3 * n + 1)
+		recur = if nextN <= memoLimit then (collatzMemo !) else collatz
+
+result = maximumBy (comparing collatz) [1..1000000]
