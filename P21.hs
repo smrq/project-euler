4cd83e88db3c554b1a64ee9d01b5f035507f2d49
Greg Smith <smr.kyu@gmail.com>
2014-01-15 18:42:58 -0600

    Improved implementation of P21 divisors

--- P21.hs
+++ P21.hs
@@ -1,14 +1,30 @@
 module P21 where
+import Data.List
 main = print result
 
-divisors x
-	| x <= 10000 = (map divisors' [0..10000] !! x)
-	| otherwise  = divisors' x
-		where divisors' x = [d | d <- [1..x-1], x `mod` d == 0]
+--divisors x
+--	| x <= 10000 = (map divisors' [0..10000] !! x)
+--	| otherwise  = divisors' x
+--		where divisors' x = [d | d <- [1..x-1], x `mod` d == 0]
+
+divisors :: Integer -> [Integer]
+divisors x = (properDivisors x) ++ [x]
+
+properDivisors :: Integer -> [Integer]
+properDivisors 0 = []
+properDivisors 1 = []
+properDivisors x = 1:(divisors' x 2)
+	where
+		divisors' :: Integer -> Integer -> [Integer]
+		divisors' x current
+			| current*current == x	= [current]
+			| current*current >  x	= []
+			| x `rem` current == 0	= (current:(divisors' x (current+1))) ++ [x `div` current]
+			| otherwise           	= divisors' x (current+1)
 
 isAmicable a =
-	let b = sum $ divisors a
-	in (a /= b) && (a == (sum $ divisors b))
+	let b = sum $ properDivisors a
+	in (a /= b) && (a == (sum $ properDivisors b))
 
 amicables = [x | x <- [1..9999], isAmicable x]
 result = sum amicables
