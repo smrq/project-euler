764571d291f24f92a30723b5d51e0b0746de6046
Greg Smith <smr.kyu@gmail.com>
2014-01-15 18:32:31 -0600

    Solved P22

---
+++ P22.hs
@@ -0,0 +1,25 @@
+module P22 where
+import Data.Char
+import Data.List
+import qualified Data.Text as T
+main = do
+	names <- readFile "P22_names.txt"
+	print $ scoreNames names
+
+removeQuotes :: T.Text -> T.Text
+removeQuotes str = T.dropAround (== '"') str
+
+parseNames :: String -> [String]
+parseNames names = map (T.unpack . removeQuotes) $ T.splitOn (T.pack ",") (T.pack names)
+
+scoreLetter :: Char -> Int
+scoreLetter letter = ord (toUpper letter) - ord 'A' + 1
+
+scoreLetters :: String -> Int
+scoreLetters name = sum $ map scoreLetter name
+
+scoreName :: String -> Int -> Int
+scoreName name index = index * (scoreLetters name)
+
+scoreNames :: String -> Int
+scoreNames names = sum $ zipWith scoreName (sort $ parseNames names) [1..]
