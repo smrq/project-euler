8171e2c746db9991746ec85b7c2e46e2f5b19635
Greg Smith <smr.kyu@gmail.com>
2014-01-22 12:36:58 -0600

    Solved P36

---
+++ P36.hs
@@ -0,0 +1,13 @@
+module P36 where
+
+toBase :: Int -> Int -> [Int]
+toBase b n
+	| n < b = [n]
+	| otherwise = (toBase b $ n `div` b) ++ [n `mod` b]
+
+isPalindrome arr = reverse arr == arr
+isDecimalPalindrome n = isPalindrome $ toBase 10 n
+isBinaryPalindrome n = isPalindrome $ toBase 2 n
+
+palindromes = [n | n <- [1..999999], isDecimalPalindrome n, isBinaryPalindrome n]
+result = sum palindromes
