7d104054be19119ee3d2646675bd6fcf14b2bce0
Greg Smith <smr.kyu@gmail.com>
2016-06-16 03:49:39 -0500

    Solved P136

---
+++ racket/P136.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+; x^2 - y^2 - z^2 = n
+; let: x = a + 2k
+;      y = a + k
+;      z = a
+; 3k^2 + 2ak - a^2 = n
+; (3k-a)(k+a) = n
+;  -> a < 3k
+
+; N is at maximum at a=k
+
+(define (N k a)
+  (+ (* 3 k k) (* 2 a k) (* -1 a a)))
+
+(define limit (* 50 (expt 10 6)))
+(define v (make-vector limit))
+
+(for ([k (in-naturals 1)]
+      #:break (>= (N k (sub1 (* 3 k))) limit))
+
+  (for ([a (in-range (sub1 (* 3 k)) (sub1 k) -1)]
+        #:break (>= (N k a) limit))
+    (let ([n (N k a)])
+      (vector-set! v n (add1 (vector-ref v n)))))
+
+  (for ([a (in-range 1 k)]
+        #:break (>= (N k a) limit))
+    (let ([n (N k a)])
+      (vector-set! v n (add1 (vector-ref v n))))))
+
+(for/sum ([count v])
+  (if (= count 1) 1 0))
